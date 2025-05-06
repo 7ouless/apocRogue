75245e8639a3e37dc0fe1155400cd5dbb182616f
ALEX <AlexNitro52525@gmail.com>
2025-05-06 22:15:27 +0100

    re adding platformtile that got deleted in the merge... i love merging

---
+++ core/src/main/java/io/github/apocRogue/map/PlatformTile.java
@@ -0,0 +1,8 @@
+package io.github.apocRogue.map;
+
+import com.badlogic.gdx.graphics.Color;
+public class PlatformTile extends TileActor {
+    public PlatformTile(float x, float y, float width, float height) {
+        super(x, y, width, height, Color.FOREST);
+    }
+}
