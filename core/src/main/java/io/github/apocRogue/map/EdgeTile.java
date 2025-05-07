0870225925e6a8ea9e516e4096f8f011af1e61ae
ALEX <AlexNitro52525@gmail.com>
2025-05-07 18:09:47 +0100

    Checkpoint, added edge image to to edge

--- core/src/main/java/io/github/apocRogue/map/EdgeTile.java
+++ core/src/main/java/io/github/apocRogue/map/EdgeTile.java
@@ -1,9 +1,21 @@
 package io.github.apocRogue.map;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 public class EdgeTile extends TileActor {
+    private static final Texture tex = new Texture("ui/edge.png");
+
     public EdgeTile(float x, float y, float size) {
         super(x, y, size, size, Color.PURPLE);
+        setSize(size, size);
     }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(tex, getX(), getY(), getWidth(), getHeight());
+    }
+
+
 }
--- core/src/main/java/io/github/apocRogue/map/FloorTile.java
+++ core/src/main/java/io/github/apocRogue/map/FloorTile.java
@@ -1,9 +1,20 @@
 package io.github.apocRogue.map;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
-// Solid ground tile
 public class FloorTile extends TileActor {
+    private static final Texture tex = new Texture("ui/floor.png");
+
     public FloorTile(float x, float y, float width, float height) {
-        super(x, y, width, height, Color.BROWN);
-    }}
+        super(x, y, width, height, Color.GREEN);   // ← call the TileActor ctor
+        // (we won’t actually use the Color fill, since we override draw below)
+        setSize(width, height);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(tex, getX(), getY(), getWidth(), getHeight());
+    }
+}
